75d0f3d2a78bd78f081d22a03ebdfb5388b40f03
Tom Hohl <tom.hohl@gmail.com>
2021-09-23 15:37:33 -0600

    comment controller started

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,32 @@
 class Api::CommentsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_post
+    before_action :set_comment, only: [:update, :destroy]
+
+    def index
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def set_post
+        @post = current_user.posts.find(params[:post_id])
+    end
+
+    def set_comment
+        @comment = @post.comments.find(params[:id])
+    end
+
+    def comment_params
+        
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     get 'things', to: 'things#index'
-    resources :posts
+    resources :posts do
+      resources :comments
+    end
   end
   
 end
