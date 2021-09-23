e8da1c6f939e2b6364ca81172bdd8dce7bbdbf0a
Elise Brown <elisemarie88@gmail.com>
2021-09-23 16:38:15 -0600

    User page posts rendered

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -1,15 +1,43 @@
-import React, { useContext } from 'react'
+import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react'
+import { Card, Header } from 'semantic-ui-react'
+import Post from '../components/Post'
 import PostForm from '../components/PostForm'
 import User from '../components/User'
 import { AuthContext } from '../providers/AuthProvider'
 
 const UserPage = () => {
-    const {user} = useContext(AuthContext)
+    const [posts, setPosts] = useState("")
+    const { user } = useContext(AuthContext)
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+
+    const getPosts = async () => {
+        try {
+            let res = await axios.get("/api/posts")
+            setPosts(res.data)
+        } catch (error) {
+            alert('error')
+        }
+    }
+
+    const renderPosts = () => {
+        if (posts.length == 0) {
+            return <p>No Posts</p>
+        }
+        return posts.map((p) => 
+            <Post post={p}/>
+            )
+    }
+
     
     return (
         <>
-            <User user={user}/>
+            <User user={user} />
             <PostForm />
+            {renderPosts()}
         </>
     )
 }
