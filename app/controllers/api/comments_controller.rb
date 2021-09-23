45492cabb5a6a0c78e3bdd61bfba6788ca2c422d
Tom Hohl <tom.hohl@gmail.com>
2021-09-23 16:04:22 -0600

    comments controller, untested

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -4,15 +4,28 @@ class Api::CommentsController < ApplicationController
     before_action :set_comment, only: [:update, :destroy]
 
     def index
+        @post.comments.find(params[:id])
     end
 
     def create
+        @comment = @post.comments.new(comment_params)
+        if(@comment.save)
+            render json: @comment
+        else
+            render json: @comment.errors, status: 422
+        end
     end
 
     def update
+        if(@comment.update(comment_params))
+            render json: @comment
+        else
+            render json: @comment.errors, status: 422
+        end
     end
 
     def destroy
+        @comment.destroy
     end
 
     private
@@ -26,7 +39,7 @@ class Api::CommentsController < ApplicationController
     end
 
     def comment_params
-        
+        params.require(:comment).permit(:text, :name)
     end
 
 end
